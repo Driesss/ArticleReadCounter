c16e3d8a520f4739cd5f21e95f9f6b6fcbe32ffd
dries <hello@driesstelten.dev>
2020-11-19 12:56:26 +0100

    add /page endpoint

--- api/articles.js
+++ api/articles.js
@@ -6,11 +6,20 @@ module.exports = function (fastify, opts, done) {
         reply.code(204)
     })
     fastify.get('/page', async (request, reply) => {
-        var skip = 30 * (request.query.page - 1)
+        const pagesize = 30
+        var skip = pagesize * (request.query.page - 1)
         const client = await fastify.pg.connect()
-        const { rows } = await client.query('SELECT * FROM articles WHERE id > $1 ORDER BY read_count desc LIMIT 30', [skip])
+        var result
+        if (request.query.search) {
+            result = await client.query('SELECT * FROM articles WHERE id > $1 AND title LIKE $2 ORDER BY read_count desc LIMIT $3', [skip, `%${request.query.search}%`, pagesize])
+        } else {
+            result = await client.query('SELECT * FROM articles WHERE id > $1 ORDER BY read_count desc LIMIT $2', [skip, pagesize])
+        }
+        const rowQuery = await client.query('SELECT COUNT(id) FROM articles')
+        const rowCount = rowQuery.rows[0].count;
+        const pageCount = Math.ceil(rowCount / pagesize)
         client.release()
-        return { results: rows }
+        return { results: result.rows, pageCount, currentPage: request.query.page }
 
     })
     fastify.get('/', async (request, reply) => {
